e5a9b4f7c097a7c0fa511873ccd555d5c9cd2fa5
julienbutty <julien.butty@360medics.com>
2023-06-13 21:26:03 +0200

    :lipstick: Set Dracula theme

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "Nightfox",
+  colorscheme = "dracula",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
